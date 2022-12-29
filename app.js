d06b47b9d19dcb392433b1e3ebc6b581e0cdb518
Srijan <100436695+TruecoderSri@users.noreply.github.com>
2022-12-29 15:34:54 +0530

    Add files via upload
    everyday todo-List

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const app = express();
+
+var items = ["get-up", "go for a walk", "get breakfast done"];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  let day = date.getDate();
+
+  res.render("list", { dayOfTheWeek: day, newListItem: items });
+});
+
+app.post("/", (req, res) => {
+  var op = req.body.newItem;
+  items.push(op);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
